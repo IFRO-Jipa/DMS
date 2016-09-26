831fe3cba60bc278f8aee88dc68e93a2ac5d4ad8
Leonardo Braz <Leonardo Braz>
2016-09-26 09:10:37 -0400

    Modificando as matrizes

--- src/main/java/br/com/dms/control/MatrizController.java
+++ src/main/java/br/com/dms/control/MatrizController.java
@@ -4,23 +4,28 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import br.com.dms.util.AlertAdapter;
+import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.SplitPane;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 public class MatrizController implements Initializable {
 
 	@FXML
 	private SplitPane container;
+
+	@FXML
+	private SplitPane containerResultado;
 
 	@FXML
 	private RadioButton rbOperacaoSubtracao;
@@ -33,6 +38,15 @@ public class MatrizController implements Initializable {
 
 	@FXML
 	private Button btnMontar;
+
+	@FXML
+	private GridPane gridMatriz1;
+
+	@FXML
+	private GridPane gridMatriz2;
+
+	@FXML
+	private Button btnCalcular;
 
 	@FXML
 	private AnchorPane header;
@@ -49,41 +63,115 @@ public class MatrizController implements Initializable {
 	@FXML
 	private TextField txtColunaM2;
 
-	@FXML
-	private TableView<String> tbMatriz1;
-
-	@FXML
-	private TableView<String> tbMatriz2;
-
-	@FXML
-	private TableView<String> tbResultado;
-
 	private ToggleGroup grupoOperacoes;
 
 	@FXML
 	private void handleMontaMatriz(ActionEvent event) {
 		if (isMatrizesConfiguradas() && hasOperacao()) {
-			this.container.setVisible(true);
 
-			this.criaMatriz(tbMatriz1, Integer.parseInt(txtLinhaM1.getText()), Integer.parseInt(txtColunaM1.getText()));
-			this.criaMatriz(tbMatriz2, Integer.parseInt(txtLinhaM2.getText()), Integer.parseInt(txtColunaM2.getText()));
+			if (valoresValidosParaOperacao()) {
+				if (!this.container.isVisible()) {
+					this.container.setVisible(true);
+					this.container.getItems().remove(containerResultado);
+				}
+
+				this.criaMatriz(this.gridMatriz1, Integer.parseInt(txtLinhaM1.getText()),
+						Integer.parseInt(txtColunaM1.getText()));
+				this.criaMatriz(this.gridMatriz2, Integer.parseInt(txtLinhaM2.getText()),
+						Integer.parseInt(txtColunaM2.getText()));
+
+				this.btnCalcular.setVisible(true);
+			}
+
 		} else {
 			AlertAdapter.warning("Dados necessários",
 					"Informe o tipo de operação e as dimensões das duas matrizes para prosseguir");
 
 			this.container.setVisible(false);
+			this.btnCalcular.setVisible(false);
 		}
 	}
 
-	private void criaMatriz(TableView<String> tbMatriz, int linhas, int colunas) {
-		tbMatriz.getColumns().clear();
-		tbMatriz.getColumns().add(new TableColumn<>());
+	private void criaMatriz(GridPane gridPane, int rows, int columns) {
+		gridPane.getChildren().clear();
+		for (int row = 0; row < rows; row++) {
+			for (int column = 0; column < columns; column++) {
+				TextField text = new TextField("0");
+				text.setPrefColumnCount(5);
+				text.textProperty().addListener((ChangeListener<String>) (observable, oldValue, newValue) -> {
+					if (!newValue.matches("\\d*")) {
+						text.setText(newValue.replaceAll("[^\\d]", ""));
+					}
+				});
+				text.setId(String.format("%d,%d", row, column));
+
+				gridPane.add(text, column, row);
+				GridPane.setMargin(text, new Insets(5, 5, 5, 5));
+
+			}
+		}
 	}
 
-	@Override
+	@FXML
+	private void handleBtnCalcula(ActionEvent event) {
+
+	}
+
+	private boolean valoresValidosParaOperacao() {
+		Toggle selecao = grupoOperacoes.getSelectedToggle();
+
+		if (selecao == this.rbOperacaoSoma) {
+			return validaOperacaoSomaSubtracao();
+		} else if (selecao == this.rbOperacaoMultiplicacao) {
+			return validaOperacaoMultiplicacao();
+		} else if (selecao == this.rbOperacaoSubtracao) {
+			return validaOperacaoSomaSubtracao();
+		}
+
+		return false;
+	}
+
+	private boolean validaOperacaoMultiplicacao() {
+		int linhaM2 = Integer.parseInt(this.txtLinhaM2.getText());
+		int colunaM1 = Integer.parseInt(this.txtColunaM1.getText());
+
+		if (linhaM2 == colunaM1) {
+			return true;
+		} else {
+			AlertAdapter.error("Valores inválidos",
+					"Para realizar uma multiplicação, o número de colunas da Matriz 1 deve ser "
+							+ "igual ao número de linhas da Matriz 2.\n\nExemplo:\n\t3x2 2x3 -> VÁLIDO"
+							+ "\n\t3x4 3x4 -> INVÁLIDO");
+			return false;
+		}
+	}
+
+	private boolean validaOperacaoSomaSubtracao() {
+		int colunaM1 = Integer.parseInt(this.txtColunaM1.getText());
+		int linhaM1 = Integer.parseInt(this.txtLinhaM1.getText());
+
+		int linhaM2 = Integer.parseInt(this.txtLinhaM2.getText());
+		int colunaM2 = Integer.parseInt(this.txtColunaM2.getText());
+
+		if ((colunaM1 == colunaM2) && (linhaM1 == linhaM2)) {
+			return true;
+		} else {
+			AlertAdapter.error("Valores inválidos",
+					"Para realizar uma soma ou subtração, as matrizes devem possuir a mesma ordem");
+			return false;
+		}
+	}
+
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		initContainers();
+		this.btnCalcular.setVisible(false);
 		initGrupoOperacoes();
 		initButtonMontar();
+	}
+
+	private void initContainers() {
+		this.container.setVisible(false);
+		this.containerResultado.setVisible(false);
 	}
 
 	private void initGrupoOperacoes() {
@@ -92,19 +180,16 @@ public class MatrizController implements Initializable {
 	}
 
 	private void initButtonMontar() {
-		btnMontar.disabledProperty().addListener((obs, oldV, newV) -> {
-			btnMontar.setDisable(!(isMatrizesConfiguradas() && hasOperacao()));
-		});
 
 		btnMontar
 				.setTooltip(new Tooltip("Prepara as matrizes para inserção dos valores a partir dos dados definidos."));
 	}
 
 	private boolean isMatrizesConfiguradas() {
-		boolean matriz1 = txtLinhaM1.getText().isEmpty() && txtColunaM1.getText().isEmpty();
-		boolean matriz2 = txtLinhaM2.getText().isEmpty() && txtColunaM2.getText().isEmpty();
+		boolean matriz1 = !txtLinhaM1.getText().isEmpty() && !txtColunaM1.getText().isEmpty();
+		boolean matriz2 = !txtLinhaM2.getText().isEmpty() && !txtColunaM2.getText().isEmpty();
 
-		return matriz1 && matriz2;
+		return matriz1 == true && matriz2 == true;
 	}
 
 	private boolean hasOperacao() {
--- src/main/test/matriz/TableViewSample.java
+++ src/main/test/matriz/TableViewSample.java
@@ -1,19 +1,12 @@
 package matriz;
 
-import java.util.Arrays;
-
+import br.com.dms.util.view.TableViewUtil;
 import javafx.application.Application;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.util.Callback;
 
 public class TableViewSample extends Application {
 
@@ -24,32 +17,24 @@ public class TableViewSample extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		StackPane root = new StackPane();
-		String[][] staffArray = new String[15][15];
-		for (int i = 0; i < staffArray.length; i++) {
-			for (int j = 0; j < staffArray[i].length; j++) {
-				staffArray[i][j] = "0.0";
-			}
-		}
-		ObservableList<String[]> data = FXCollections.observableArrayList();
-		data.addAll(Arrays.asList(staffArray));
-		data.remove(0);// remove titles from data
-		TableView<String[]> table = new TableView<>();
-		table.setEditable(true);
-		for (int i = 0; i < staffArray[0].length; i++) {
-			TableColumn<String[], String> tc = new TableColumn<>(staffArray[0][i]);
-			final int colNo = i;
-			tc.setCellValueFactory(new Callback<CellDataFeatures<String[], String>, ObservableValue<String>>() {
-				@Override
-				public ObservableValue<String> call(CellDataFeatures<String[], String> p) {
-					return new SimpleStringProperty((p.getValue()[colNo]));
+
+		String[][] array = TableViewUtil.createAndPopulateArray(3, 3, "0");
+
+		TableView<String[]> table = TableViewUtil.getTableViewForArray(array, true);
+
+		root.getChildren().add(table);
+		Button btn = new Button("Mostrar");
+		btn.setOnAction(ev -> {
+			table.getItems().forEach(items -> {
+				for (String item : items) {
+					System.out.println(item);
+
 				}
 			});
-			tc.setPrefWidth(90);
-			table.getColumns().add(tc);
-		}
-		table.setItems(data);
-		root.getChildren().add(table);
+		});
+		root.getChildren().add(btn);
 		primaryStage.setScene(new Scene(root, 300, 250));
 		primaryStage.show();
 	}
+
 }
